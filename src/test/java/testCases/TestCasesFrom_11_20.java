183b9c794029bfa6f4536c22bba256fd6e3769de
Ajay Kadam <ajkadamm@gmail.com>
2024-10-10 23:02:46 +0530

    User1 first Commit

--- src/test/java/testCases/TestCasesFrom_11_20.java
+++ src/test/java/testCases/TestCasesFrom_11_20.java
@@ -1,9 +1,11 @@
 package testCases;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class TestCasesFrom_11_20 
 {
+	WebDriver driver;
 	@Test
 	public void m11() 
 	{
@@ -26,5 +28,7 @@ public class TestCasesFrom_11_20
 	public void m14() 
 	{
 		System.out.println("TestCase14");
+		///TakesScreenshot screen=(TakesScreenshot)driver;
+		// String src = screen.getScreenshotAs(OutputType.BASE64);
 	}
 }
